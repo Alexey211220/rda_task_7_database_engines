c2bd0118f2a90ba5e80e6c1fc446abb02937b7e7
alexey211204 <alexey.zhuravel2004@gmail.com>
2026-07-07 21:47:05 +0200

    Sol 4

--- task.sql
+++ task.sql
@@ -37,7 +37,7 @@ CREATE TABLE Logs (
 
 -- Creating a table for storing reporting data, which will be send to a separate application in the CSV format for analytics purposes (Columns:  Date, ProductName, Orders)
 CREATE TABLE ProductReporting (
-    Date DATE,
-    ProductName VARCHAR(100),
-    Orders INT
+    Date DATE NOT NULL,
+    ProductName VARCHAR(100) NOT NULL,
+    Orders INT NOT NULL
 ) ENGINE=CSV;
